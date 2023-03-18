7740d0423bb7cb7c5490e14f403e8a5745b2ab3d
Daniil <danuul2@mail.ru>
2023-03-18 15:13:18 +0600

    parser.py and main.py

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-import requests
-from bs4 import BeautifulSoup
+import Parser
 
-def parse():
-    url = "https://www.omgtu.ru/general_information/the-structure/the-department-of-university.php"  # URL сайта для парсинга
-
-    # Отправляем GET-запрос на сайт
-    padge = requests.get(url)
-    print(padge.status_code) # смотрим ответ
-    # Инициализируем объект BeautifulSoup и передаем ответ сервера в качестве параметра
-    soup = BeautifulSoup(padge.text, "html.parser")
-    # Ищем все элементы с классом "a" и записываем содержимое в список
-    block = soup.findAll('a')
-
-    # Открываем файл для записи
-    with open("ReSuL.txt", "w", encoding="utf-8") as f:
-        # Записываем каждый элемент списка block в файл построчно
-        for data in block:
-            description = data.text.strip()
-            f.write(description + "\n")
+if __name__ == '__main__':
+    Parser.parse()
